986fedbe295f909251cbb0ade23041cbfbdc49e2
Eden Shaul <shaul.eden@gmail.com>
2023-03-13 23:41:34 +0200

    fix recipes and create structure of recommender

---
+++ src/recommendations/recommender.py
@@ -0,0 +1,49 @@
+from src.infra.postgres_connector import connect, execute_select
+
+RECIPE_COLUMNS = ['index',
+                  'recipe_title',
+                  'url',
+                  'record_health',
+                  'vote_count',
+                  'rating',
+                  'description',
+                  'cuisine',
+                  'course',
+                  'diet',
+                  'prep_time',
+                  'cook_time',
+                  'ingredients',
+                  'instructions',
+                  'author',
+                  'tags',
+                  'category',
+                  'image',
+                  'difficulty',
+                  'total_time']
+
+
+class Recommender:
+    def get_recipes(self, user_id):
+        if self._needs_cold_start(user_id):
+            return self._get_cold_start_recipes(user_id)
+
+        return self._recommend_recipes(user_id)
+
+    def _needs_cold_start(self, user_id):
+        # TODO: write cold start entering logic (e.g. if the user has less than X ratings)
+        return True
+
+    def _get_cold_start_recipes(self, user_id):
+        query = "select * from recipes;"
+        conn = connect()
+        df = execute_select(conn, query, RECIPE_COLUMNS)
+        # TODO: 1. get the overall most popular (vote count + rating)
+        #       2. sort the categories by popularity
+        #           2.1 for each category -> get the most popular (vote count + rating)
+        #       3. if there are other users in the DB
+        #           3.1 do Count Vectorizer for the user metadata to find similar users
+        #           3.2 get the other user's top rated recipes
+        conn.close()
+
+    def _recommend_recipes(self, user_id):
+        pass
