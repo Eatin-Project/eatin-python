d2f6e6007e1c2bedbae0ab2b8f96c5251ed1ca1e
Eden Shaul <shaul.eden@gmail.com>
2023-03-18 15:32:07 +0200

    added top categories

--- src/recommendations/recommender.py
+++ src/recommendations/recommender.py
@@ -2,6 +2,25 @@ import json
 
 from src.infra.postgres_connector import connect, execute_select
 
+MOST_POPULAR_QUERY = "select * from recipes order by vote_count desc, rating desc limit 100;"
+
+TOP_CATEGORIES_QUERY = "SELECT \
+  category, \
+  COUNT(*) AS recipe_count, \
+  SUM(vote_count) AS total_votes, \
+  AVG(rating) AS average_rating, \
+  (0.4 * SUM(vote_count) + 0.4 * AVG(rating) + 0.2 * COUNT(*)) AS popularity_score \
+FROM recipes \
+GROUP BY category \
+ORDER BY popularity_score DESC \
+LIMIT 10;"
+
+TOP_RECIPES_FOR_CATEGORY_QUERY = "select * from recipes \
+where category = '{}' \
+order by (vote_count * rating) desc \
+limit 13;"
+
+
 RECIPE_COLUMNS = ['index',
                   'recipe_title',
                   'url',
@@ -45,15 +64,23 @@ def _needs_cold_start(user_id):
 
 
 def _get_cold_start_recipes(user_id):
-    query = "select * from recipes order by vote_count desc, rating desc limit 100;"
     conn = connect()
-    df = execute_select(conn, query, RECIPE_COLUMNS)
-    recipes_json = json.loads(df.to_json(orient='records'))
-    recipes = [{'name': 'for_you', 'recipes': recipes_json[:13]},
-               {'name': 'other', 'recipes': recipes_json[14:99]}]
+    most_popular_df = execute_select(conn, MOST_POPULAR_QUERY, RECIPE_COLUMNS)
+    top_categories_df = execute_select(conn, TOP_CATEGORIES_QUERY,
+                                       ['category', 'recipe_count', 'total_votes', 'average_rating', 'popularity_score'])
+    category_sections = [create_sections_of(category, conn) for category in top_categories_df['category']]
+    recipes_json = json.loads(most_popular_df.to_json(orient='records'))
+    popular_section = [{'name': 'Popular On Eatin', 'recipes': recipes_json}]
     conn.close()
 
-    return recipes
+    return popular_section + category_sections
+
+
+def create_sections_of(category, conn):
+    df = execute_select(conn, TOP_RECIPES_FOR_CATEGORY_QUERY.format(category), RECIPE_COLUMNS)
+    recipes_json = json.loads(df.to_json(orient='records'))
+    section = {'name': category, 'recipes': recipes_json}
+    return section
 
 
 def _recommend_recipes(user_id):
