c892ed2c26975f4c25f0eb8f1c62336629c0d26a
Ofri Lahad <olahad24@gmail.com>
2023-03-25 21:44:30 +0300

    extracting popularity calculation to var

--- src/recommendations/consts.py
+++ src/recommendations/consts.py
@@ -12,17 +12,20 @@ GET_USER_TOP_RATED_RECIPES_QUERY = "select recipes.recipe_title from ratings, re
 
 MOST_POPULAR_QUERY = "select * from recipes order by vote_count desc, rating desc limit 20;"
 
+POPULARITY = "(0.4 * SUM(vote_count) + 0.4 * AVG(rating) + 0.2 * COUNT(*))"
+
 TOP_CATEGORIES_QUERY = "SELECT \
   category, \
   COUNT(*) AS recipe_count, \
   SUM(vote_count) AS total_votes, \
   AVG(rating) AS average_rating, \
-  (0.4 * SUM(vote_count) + 0.4 * AVG(rating) + 0.2 * COUNT(*)) AS popularity_score, \
-  ROW_NUMBER () OVER (ORDER BY (0.4 * SUM(vote_count) + 0.4 * AVG(rating) + 0.2 * COUNT(*)) desc) as row_num \
+  {} AS popularity_score, \
+  ROW_NUMBER () OVER (ORDER BY {} desc) as row_num \
 FROM recipes \
 GROUP BY category \
 ORDER BY popularity_score DESC \
-LIMIT 10;"
+LIMIT 10;".format(POPULARITY, POPULARITY)
+
 
 TOP_RECIPES_FOR_CATEGORY_QUERY = "select * from recipes \
 where category = '{}' \
