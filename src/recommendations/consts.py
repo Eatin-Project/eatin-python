6d511c90f8bcb4a9430e6f459e3d0bf179cf506d
Eden Shaul <shaul.eden@gmail.com>
2023-05-02 23:00:36 +0300

    changed the model input

--- src/recommendations/consts.py
+++ src/recommendations/consts.py
@@ -3,8 +3,9 @@ import os
 
 import pandas as pd
 
-RECIPE_AMOUNT = 3
+RECIPE_AMOUNT = 13
 COLD_START_RATING_AMOUNT = 3
+RATING_LOWER_BOUND = 3.5
 
 COUNT_USER_RATINGS_QUERY = "select count(*) from ratings where user_id = '{}'"
 ALL_RECIPES_QUERY = "select * from recipes"
@@ -13,7 +14,8 @@ USER_RATINGS_COLUMNS = ['user_id', 'recipe_index', 'rating', 'rating_timestamp']
 GET_USER_TOP_RATED_RECIPES_QUERY = "select recipes.recipe_title from ratings, recipes \
                                         where ratings.user_id = '{}' \
                                         and ratings.recipe_index = recipes.index \
-                                        order by ratings.rating desc \
+                                        and ratings.rating >= {} \
+                                        order by ratings.rating_timestamp desc, ratings.rating desc \
                                         limit {}"
 
 MOST_POPULAR_QUERY = "select * from recipes order by vote_count desc, rating desc limit 20;"
@@ -32,12 +34,10 @@ GROUP BY category \
 ORDER BY popularity_score DESC \
 LIMIT 10;".format(POPULARITY)
 
-
 TOP_RECIPES_FOR_CATEGORY_QUERY = "select * from recipes \
 where category = '{}' \
 order by (vote_count * rating) desc \
 limit 20;"
-
 
 TOP_CATEGORIES_COLUMNS = ['category', 'recipe_count', 'total_votes', 'average_rating', 'popularity_score', 'row_num']
 RECIPE_COLUMNS = ['index',
@@ -60,7 +60,6 @@ RECIPE_COLUMNS = ['index',
                   'image',
                   'difficulty',
                   'total_time']
-
 
 TF_IDF_FILE_LOCATION = os.path.join('models', 'tf_idf.joblib')
 COUNT_VECTORIZER_FILE_LOCATION = os.path.join('models', 'count-vectorizer.joblib')
